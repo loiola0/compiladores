4076f9c014b461dd475ce6b8b568db2aed3dd3d0
Victor Gabriel <61810592+loiola0@users.noreply.github.com>
2024-04-10 21:19:15 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ var input = "A4 * (12.4 + 4) * 12.3.4 / (2/5) * 2.3 * X + 4.8 (10/.5) + 9. / 2.3
 
 var lexer = new Lexer(input);
 
-var tokens = lexer.Casamento();
+var tokens = lexer.Casar();
 
 foreach (var token in tokens)
 {
